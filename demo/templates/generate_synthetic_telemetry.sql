82492870e920472bfc838496a4ce8785fbb5622c
cccs-jc <cccs-jc@cyber.gc.ca>
2023-02-28 20:39:24 +0000

    formated sql

--- demo/templates/generate_synthetic_telemetry.sql
+++ demo/templates/generate_synthetic_telemetry.sql
@@ -1,18 +1,18 @@
 
 select
-   e.col1 as timestamp,
+   event.ts as timestamp,
    host_id,
-   e.col2 as id,
-   e.col3 as parent_id,
+   event.id,
+   event.parent_id,
    captured_folder_colname,
    Name,
    ImagePath,
-   e.col4 as Commandline
+   event.Commandline
 from
 (
     select
         *,
-        explode(events) as e
+        explode(gen_events) as event
     from (
         select
             'host-' || floor(rand() * 5000) as host_id,
@@ -23,28 +23,28 @@ from
             case 
             when value % 100000 = 0 then
                 array(
-                    struct(cast(ts - 0.002 as timestamp), 'to' || value, '0', 'temporal_ordered_a'),
-                    struct(cast(ts - 0.002 as timestamp), 't' || value, '0', 'temporal_c'),
-                    struct(cast(ts - 0.001 as timestamp), 'tt' || value, '0', 'temporal_b'),
-                    struct(cast(ts - 0.001 as timestamp), 'tto' || value, '0', 'temporal_ordered_b'),
-                    struct(cast(ts as timestamp), 'ttt' || value, '0', 'temporal_a'),
-                    struct(cast(ts as timestamp), 'ttto' || value, '0', 'temporal_ordered_c')
+                    struct(cast(cast(timestamp as long) - 0.002 as timestamp) as ts,  'to' || value as id, '0' as parent_id, 'temporal_ordered_a' as Commandline),
+                    struct(cast(cast(timestamp as long) - 0.002 as timestamp) as ts,   't' || value as id, '0' as parent_id,         'temporal_c' as Commandline),
+                    struct(cast(cast(timestamp as long) - 0.001 as timestamp) as ts,  'tt' || value as id, '0' as parent_id,         'temporal_b' as Commandline),
+                    struct(cast(cast(timestamp as long) - 0.001 as timestamp) as ts, 'tto' || value as id, '0' as parent_id, 'temporal_ordered_b' as Commandline),
+                    struct(                                         timestamp as ts, 'ttt' || value as id, '0' as parent_id,         'temporal_a' as Commandline),
+                    struct(                                         timestamp as ts,'ttto' || value as id, '0' as parent_id, 'temporal_ordered_c' as Commandline)
                 )
             when value % 50000 = 0 then
                 array(
-                    struct(cast(ts - 0.002 as timestamp), 'aaa' || value, '0', 'ancestor_a'),
-                    struct(cast(ts - 0.001 as timestamp), 'aa' || value, 'aaa' || value, 'ancestor_b'),
-                    struct(cast(ts - 0.001 as timestamp), 'pp' || value, '0', 'parent_a parent_b'),
-                    struct(cast(ts as timestamp), 'a' || value, 'aa' || value, 'ancestor_x'),
-                    struct(cast(ts as timestamp), 'p' || value, 'pp' || value, 'parent_x')
+                    struct(cast(cast(timestamp as long) - 0.002 as timestamp) as ts,  'aaa' || value as id,            '0' as parent_id,        'ancestor_a' as Commandline),
+                    struct(cast(cast(timestamp as long) - 0.001 as timestamp) as ts,   'aa' || value as id, 'aaa' || value as parent_id,        'ancestor_b' as Commandline),
+                    struct(cast(cast(timestamp as long) - 0.001 as timestamp) as ts,   'pp' || value as id,            '0' as parent_id, 'parent_a parent_b' as Commandline),
+                    struct(                                         timestamp as ts,    'a' || value as id,  'aa' || value as parent_id,        'ancestor_x' as Commandline),
+                    struct(                                         timestamp as ts,    'p' || value as id,  'pp' || value as parent_id,          'parent_x' as Commandline)
                 )
             else 
                 array(struct(
-                    cast(ts as timestamp),
-                    cast(floor(rand() * 100000) as string),
-                    cast(floor(rand() * 100000) as string),
-                    uuid()))
-            end as events
+                    timestamp                              as ts,
+                    cast(floor(rand() * 100000) as string) as id,
+                    cast(floor(rand() * 100000) as string) as parent_id,
+                    uuid()                                 as Commandline ))
+            end as gen_events
         from
             rate_view
         )
