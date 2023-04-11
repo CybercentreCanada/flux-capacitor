7fa0f62a111c3950a32d833d908de48cda88358f
cccs-jc <cccs-jc@cyber.gc.ca>
2023-04-11 12:00:24 +0000

    testing main

--- demo/main.py
+++ demo/main.py
@@ -1,7 +1,25 @@
 import time
+import sys
+import time
+
+from constants import init_argparse
+import constants
+from util import (
+    get_checkpoint_location,
+    get_spark,
+    create_spark_session,
+    create_view,
+    make_name,
+    monitor_query,
+    print_anomalies,
+    render_statement,
+    validate_events,
+    run
+)
 
 def run():
     print('inside run')
+    create_spark_session("streaming alert builder", 1)
     time.sleep(15)
     raise Exception("this is an exception")
 
